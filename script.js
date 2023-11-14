7b68bee563c96a0d32f7f0739ece002a78f59537
Mohamed <100893858+mob34@users.noreply.github.com>
2023-11-14 19:13:49 +0100

    added some functions and styles

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-const button = document.getElementById("button");
-const show = document.getElementById("list");
-
 function shaw() {
-  const input = document.getElementById("todo").value;
-  console.log(input);
+  const input = document.getElementById("todo");
   const li = document.createElement("li");
-  li.innerText = input;
-  document.body.appendChild(li);
+
+  if (input.value === "") {
+    //if input = "" then alert
+    alert("Please write something");
+  } else {
+    li.innerText = input.value;
+    const btn = document.createElement("button");
+    btn.textContent = "Delete";
+    btn.setAttribute("class", "del");
+
+    //Adding btn to <li>
+    li.appendChild(btn);
+
+    //Adding <li> to <ul>
+    const list = document.getElementById("list");
+    list.appendChild(li);
+
+    //Clearing the input bar after clicking on the button 'add'
+    input.value = "";
+
+    //Delete the <li> onclick
+    btn.addEventListener("click", function () {
+      this.parentNode.remove();
+    });
+  }
 }
